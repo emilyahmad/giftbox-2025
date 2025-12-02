4093214f54eb961d9b2c7c32ba27816a961b3f49
Emily <emilyahmad77@gmail.com>
2025-12-02 10:21:10 -0500

    Adding some javascript for the falling effect

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+document.getElementById("tag").addEventListener("click", fallingTag);
+
+function fallingTag() {
+  document.getElementById("tag").style.display = "none";
 
 
+  
 
-
-
-
+}
 
 document.getElementById("instruction-p").innerHTML = "";
