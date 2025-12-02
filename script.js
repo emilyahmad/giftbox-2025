8a2ab26fe31e785de9e56cf41d3342a26f13ba4e
Emily <emilyahmad77@gmail.com>
2025-12-02 10:52:01 -0500

    Falling tag works

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-document.getElementById("tag").addEventListener("click", fallingTag);
+const tagBox = document.querySelector(".tag-box");
 
+const tagBox = document.querySelector(".tag-box");
+tagBox.addEventListener("click", fallingTag);
 function fallingTag() {
-  document.getElementById("tag").style.display = "none";
+    let y = tagBox.offsetTop;
 
+    function fall() {
+        y += 5;
+        tagBox.style.top = y + "px";
+        tagBox.style.transform = "translate(-50%, -50%) rotate(20deg)";
 
-  
+        const rect = tagBox.getBoundingClientRect();
+        if (rect.top > window.innerHeight) {
+            tagBox.style.display = "none";
+            return;
+        }
+
+        requestAnimationFrame(fall);
+    }
+
+    fall();
 
 }
-
-document.getElementById("instruction-p").innerHTML = "";
